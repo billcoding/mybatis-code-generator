c8ab621be17e63d7c3fef71d45419638fb0957b0
billcodng <wangmiao07@sina.com>
2021-03-05 11:08:02 +0800

    Update

--- help.go
+++ help.go
@@ -9,13 +9,13 @@ import (
 func printUsage() {
 	fmt.Printf(`Usage of mybatis-code-generator:
 
-mybatis-code-generator -OPTIONS
+mybatis-code-generator -dsn DSN -db DATABASE -OPTIONS
 
 Examples:
 
-mybatis-code-generator  -au "bigboss" -dsn "root:123@tcp(127.0.0.1:3306)/test"
-mybatis-code-generator  -au "bigboss" -ep "x.y.z.entity" -mp "x.y.z.mapper" -xd "xml" -dsn "root:123@tcp(127.0.0.1:3306)/test"
-mybatis-code-generator  -au "bigboss" -ep "x.y.z.entity" -mp "x.y.z.mapper" -xd "xml" -o "/to/path" -dsn "root:123@tcp(127.0.0.1:3306)/test"
+mybatis-code-generator -dsn "root:123@tcp(127.0.0.1:3306)/test" -db "Database"
+mybatis-code-generator -dsn "root:123@tcp(127.0.0.1:3306)/test" -db "Database" -o "/to/path" 
+mybatis-code-generator -dsn "root:123@tcp(127.0.0.1:3306)/test" -db "Database" -au "bigboss" -o "/to/path" 
 
 Supports options:
 `)
